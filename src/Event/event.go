508cac015a8b237d37ff61c977b04c4566cf06e4
Arthur <arthur.morelon@gmail.com>
2023-12-26 12:03:03 +0100

    WIP Get Events

--- src/Event/event.go
+++ src/Event/event.go
@@ -1,13 +1,16 @@
 package Event
 
 import (
+	"database/sql"
 	"fmt"
+	"main/src/color"
 	"main/src/input"
 	"regexp"
 	"time"
 )
 
 type Event struct {
+	Id          int
 	Title       string
 	StartDate   time.Time
 	EndDate     time.Time
@@ -53,7 +56,10 @@ func CreateEvent() (*Event, error) {
 
 	fmt.Print("Entrez une brève description: ")
 	event.Description = input.InputString()
-
+	res := confirmEvent(event)
+	if !res {
+		// a finir appeler updateEvent pour modifier 1 attribut
+	}
 	return event, nil
 }
 
@@ -69,4 +75,28 @@ func valideDate(dateString string) (time.Time, error) {
 	return date, nil
 }
 
-//func writeEvent() Event {}
+func confirmEvent(event *Event) bool {
+
+	fmt.Printf("\nTitre de l'événement:"+color.Blue+" %s"+color.Reset, event.Title)
+	fmt.Printf("\nDate de début:"+color.Blue+" %s"+color.Reset, event.StartDate.Format("2006-01-02 15:04"))
+	fmt.Printf("\nDate de fin:"+color.Blue+" %s"+color.Reset, event.EndDate.Format("2006-01-02 15:04"))
+	fmt.Printf("\nLieu:"+color.Blue+" %s"+color.Reset, event.Location)
+	fmt.Printf("\nCatégorie:"+color.Blue+" %s"+color.Reset, event.Tag)
+	fmt.Printf("\nDescription:"+color.Blue+" %s\n"+color.Reset, event.Description)
+
+	for {
+		fmt.Println("\ninformation correct ?")
+		res := input.InputString()
+		if res == "yes" {
+			return true
+		}
+		if res == "no" {
+			return false
+		}
+		fmt.Println("valeur incorrecte")
+	}
+}
+
+func GetEvents(db *sql.DB) []Event {
+	return []Event{}
+}
